a90309f900acf5d5908149763d7093b9be8811e2
pmnk <70025853+yohanperez@users.noreply.github.com>
2021-03-15 07:30:47 -0500

    Add files via upload
    Implementación de los procesos vistos en clase, para un proyecto.

---
+++ Proyecto/src/main/java/test/login.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.registry.infomodel.User;
+import jpa.Entity.Usuarios;
+import jpa.controlador.UsuariosJpaController;
+
+/**
+ *
+ * @author Administrator
+ */
+@WebServlet(name = "login", urlPatterns = {"/login"})
+public class login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String correo = request.getParameter("correo");
+            String contrasena = request.getParameter("contrasena");
+            UsuariosJpaController ujc = new UsuariosJpaController();
+            
+            Usuarios us = new Usuarios();
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("my_persistence_unit");
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction et = em.getTransaction();
+            et.begin();
+            Query query = em.createQuery("SELECT u FROM Usuarios u WHERE u.correo = :email AND u.contrasena = :password");
+            query.setParameter("email", correo);
+            query.setParameter("password", contrasena);
+             List<Usuarios> exist= query.getResultList();
+           
+            et.commit();
+            em.close();
+            if (exist.size() > 0) {
+                
+                response.sendRedirect("Menu.jsp");
+
+            } else {
+                response.sendRedirect("Registro.jsp");
+            }
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
